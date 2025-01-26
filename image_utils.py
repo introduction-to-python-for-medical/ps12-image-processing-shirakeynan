000ae5fc914a858ddf42265a015a7ee9dd3c8c3e
shirakeynan <shirush2002@gmail.com>
2025-01-26 21:14:12 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -10,10 +10,10 @@ def load_image(file_path):
     image_array = np.array(image)
     return image_array  
 def edge_detection(image_array):
-  gray_image = np.mean(image_array, axis=2) 
-  kernelY = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])  
-  kernelx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])  
-  edgeX = convolve(gray_image, kernelx)
-  edgeY = convolve(gray_image, kernelY)
-  edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
-  return edgeMAG  
+    gray_image = np.mean(image_array, axis=2) 
+    kernelY = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])  
+    kernelx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])  
+    edgeX = convolve(gray_image, kernelx)
+    edgeY = convolve(gray_image, kernelY)
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+    return edgeMAG  
